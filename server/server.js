80378a5972ad1a0b5eb7601b46ec1ce92d4656f6
Ricci Dorian <dorian.ricci@outlook.com>
2018-09-25 15:16:07 +0200

    added db mongod

--- server/server.js
+++ server/server.js
@@ -2,11 +2,23 @@
 const express = require('express');
 const app = express();
 
+//Enable bodyParser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+//Enable CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
+
 
 var MongoClient = require('mongodb').MongoClient;
 var database = undefined;
 
-var dbUrl = 'mongodb://127.0.0.1:27017/';
+var dbUrl = 'mongodb://127.0.0.1:27017/TFE4Haiti';
 MongoClient.connect(dbUrl, function(err, db) {
     if (err) {
         throw err;
