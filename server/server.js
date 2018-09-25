418d771a2028615bb88ac6489c3838f0fe9c22af
Ricci Dorian <dorian.ricci@outlook.com>
2018-09-25 14:53:06 +0200

    Init server

---
+++ server/server.js
@@ -0,0 +1,29 @@
+
+const express = require('express');
+const app = express();
+
+
+var MongoClient = require('mongodb').MongoClient;
+var database = undefined;
+
+var dbUrl = 'mongodb://127.0.0.1:27017/';
+MongoClient.connect(dbUrl, function(err, db) {
+    if (err) {
+        throw err;
+    } else {
+        database = db;
+        console.log('MongoDB connection successful');
+    }
+});
+
+
+
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+});
+
+app.listen(3000 , function () {
+    console.log('Example app listening on port 3000!')
+});
+
+s
