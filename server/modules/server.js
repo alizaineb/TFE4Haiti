5a83297bf5262e5bd84e1b81b51ece4ada9c18e6
Ricci Dorian <dorian.ricci@outlook.com>
2018-09-25 22:45:25 +0200

    added mongoose to server + init model Utilisateur

--- server/modules/server.js
+++ server/modules/server.js
@@ -26,13 +26,15 @@
 // Built-in
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var mongoose = require('mongoose');
 
 // Custom
 var logger = require('./logger');
 var config = require('./config');
 const token = require('./token');
-var mongod = require('./mongod');
+
+
+
 
 // Routes
 
@@ -43,42 +45,50 @@ var mongod = require('./mongod');
 var app = express();
 var server;
 
+
+var _configureDB = function () {
+	//Connect to mongoDB server
+    mongoose.connect('mongodb://localhost/TFE4Haiti');
+    mongoose.set('debug', true);
+	//Require the models
+    require('./../models/utilisateur'); //HERE
+};
 /**
  * Configure application:
  *		- parse json bodies
  */
 var _configureServer = function () {
-	app.use(bodyParser.json());
-	app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
 
-	app.use(function(request, response, next) {
-		logger.info('[Server] Received request for ' + request.method + ' ' + request.path);
+    app.use(function(request, response, next) {
+        logger.info('[Server] Received request for ' + request.method + ' ' + request.path);
 
-		response.header('Access-Control-Allow-Origin', '*');
-		response.header('Access-Control-Allow-Headers', 'Authorization');
+        response.header('Access-Control-Allow-Origin', '*');
+        response.header('Access-Control-Allow-Headers', 'Authorization');
 
-		if (request.method === 'OPTIONS') {
-			response.sendStatus(200);
-			return;
-		}
-			logger.info('[Server] next()');
-		next();
-	});
+        if (request.method === 'OPTIONS') {
+            response.sendStatus(200);
+            return;
+        }
+        logger.info('[Server] next()');
+        next();
+    });
 };
 
 /**
  * Configure application routes
  */
 var _configureRoutes = function () {
-	
-	require("../routes/server")
-	require('../routes/user');
 
-	app.use("*",function(req,res){
-		res.status(404).send({message : '404'});
-	});
-	
-	logger.info('[SERVER] Routes loaded');
+    require("../routes/server")
+    require('../routes/user');
+
+    app.use("*",function(req,res){
+        res.status(404).send({message : '404'});
+    });
+
+    logger.info('[SERVER] Routes loaded');
 };
 
 /**
@@ -87,13 +97,14 @@ var _configureRoutes = function () {
  * @param callback function called when the web server is listening
  */
 var start = function (callback) {
-	_configureServer();
-	_configureRoutes();
+	_configureDB();
+    _configureServer();
+    _configureRoutes();
 
-	server = app.listen(config.server.port, config.server.host, function () {
-		logger.info('[Server] Web server listening on ' + config.server.host + ':' + config.server.port);
-		if (callback) callback();
-	});
+    server = app.listen(config.server.port, config.server.host, function () {
+        logger.info('[Server] Web server listening on ' + config.server.host + ':' + config.server.port);
+        if (callback) callback();
+    });
 };
 
 /**
@@ -102,14 +113,14 @@ var start = function (callback) {
  * @param callback function called when the web server is no more listening
  */
 var stop = function (callback) {
-	if (server && typeof server.close === 'function') {
-		server.close();
-		logger.warn('[Server] Web server no more listening on ' + config.server.host + ':' + process.env.PORT);
-		if (callback) callback();
-	} else {
-		logger.warn('[Server] Cannot stop web server listening on ' + config.server.host + ':' + process.env.PORT);
-		if (callback) callback();
-	}
+    if (server && typeof server.close === 'function') {
+        server.close();
+        logger.warn('[Server] Web server no more listening on ' + config.server.host + ':' + process.env.PORT);
+        if (callback) callback();
+    } else {
+        logger.warn('[Server] Cannot stop web server listening on ' + config.server.host + ':' + process.env.PORT);
+        if (callback) callback();
+    }
 };
 
 
@@ -123,12 +134,12 @@ exports.start = start;
 exports.stop = stop;
 
 exports.registerRoute = function(method, path, handler) {
-	logger.info('[Server] Registering route ' + method + ' ' + path);
-	return app[method.toLowerCase()](path, handler);
+    logger.info('[Server] Registering route ' + method + ' ' + path);
+    return app[method.toLowerCase()](path, handler);
 };
 
 exports.registerAuthRoute = function(method, path, handler) {
-	logger.info('[Server] Registering Auth route ');
-	app[method.toLowerCase()](path, token.validateToken);
-	return exports.registerRoute(method, path, handler);
+    logger.info('[Server] Registering Auth route ');
+    app[method.toLowerCase()](path, token.validateToken);
+    return exports.registerRoute(method, path, handler);
 };
