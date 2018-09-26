5bf16801d78ae0093ad2f9c540a781c468a7ef3c
Ricci Dorian <dorian.ricci@outlook.com>
2018-09-26 08:19:35 +0200

    corrected typographical error

--- server/models/station.js
+++ server/models/station.js
@@ -4,14 +4,14 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var station = new Schema({
     nom: { type: String, required: true},
-	latitude: { type: Number, required: true, min:-90, max; 90},
-	longitude: { type: Number, required: true, min:-180, max; 180},
+	latitude: { type: Number, required: true, min:-90, max: 90},
+	longitude: { type: Number, required: true, min:-180, max: 180},
 	type: { type: String, enum:['hydro','limni'], required: true},
 	date_creation: {type: Date, required: true, default: Date.now},
 	derniere_utilisation: {type: Date},
 	photo: {type:String}, // Url vers la photo
 	utilisateurs: {type: [String], required: true},
-	etat: {type: String, enum['attente','panne'] required: true}
+	etat: {type: String, enum: ['attente','panne'], required: true}
 });
  
  
