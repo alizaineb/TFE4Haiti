f149676bccd3708b121cfc8cbeb564757bbc58d7
Ricci Dorian <dorian.ricci@outlook.com>
2018-09-27 23:56:37 +0200

    remove console.log

--- server/controllers/UserCtrl.js
+++ server/controllers/UserCtrl.js
@@ -6,24 +6,20 @@ const jwt = require('../modules/token');
 module.exports = {
   login: function(user, res) {
 
-    UsersModel.userModel.findOne({mail: user.mail, mdp: user.mdp}, function(err, result) {
-      if(err){
-				res.status(404).send({ error: err});
-      }else{
-        logger.info("[USER] result : ", result);
+    UsersModel.userModel.findOne({ mail: user.mail, mdp: user.mdp }, function(err, result) {
+      if (err) {
+        res.status(404).send({ error: err });
+      } else {
         var token = jwt.createToken(result);
-				res.status(200).send({ "result": result, "user": user, "token" : token});
+        res.status(200).send({ "result": result, "user": user, "token": token });
       }
-		})
-    //TODO connect to mongodb
+    })
   },
 
   get: function(res) {
     UsersModel.userModel.find({}).then(function(users) {
       res.status(200).send({ message: users });
     })
-    //TODO connect to mongodb2
-
   },
   getById: function(id, res) {
     //TODO connect to mongodb
@@ -35,12 +31,10 @@ module.exports = {
 
   },
   create: function(user, res) {
-
-    console.log(user);
     let uTmp = new UsersModel.userModel();
     uTmp.mail = user.mail;
     uTmp.mdp = user.mdp;
-    uTmp.type = 'admin';
+    uTmp.type = user.type;
     uTmp.save().then(() => {
       res.status(200).send({ message: uTmp });
     })
