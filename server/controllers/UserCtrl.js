23d3fbd3443614c5343d3f819346a70d1a492051
Ricci Dorian <dorian.ricci@outlook.com>
2018-09-27 22:53:38 +0200

    Get user login from db

--- server/controllers/UserCtrl.js
+++ server/controllers/UserCtrl.js
@@ -1,18 +1,28 @@
 'use strict';
-
+var logger = require('../modules/logger');
 var UsersModel = require('./../models/users');
+const jwt = require('../modules/token');
 
 module.exports = {
   login: function(user, res) {
+
+    UsersModel.userModel.findOne({mail: user.mail, mdp: user.mdp}, function(err, result) {
+      if(err){
+				res.status(404).send({ error: err});
+      }else{
+        logger.info("[USER] result : ", result);
+        var token = jwt.createToken(result);
+				res.status(200).send({ "result": result, "user": user, "token" : token});
+      }
+		})
     //TODO connect to mongodb
-    res.status(200).send({ message: "Method to implements" });
   },
 
   get: function(res) {
     UsersModel.userModel.find({}).then(function(users) {
       res.status(200).send({ message: users });
     })
-    //TODO connect to mongodb
+    //TODO connect to mongodb2
 
   },
   getById: function(id, res) {
