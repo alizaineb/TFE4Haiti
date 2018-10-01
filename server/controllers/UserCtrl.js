a001d85dcce02cd50d4c0cbbdba8cec95bb15d60
Unknown <gaetan.ansotte@hotmail.com>
2018-10-01 10:38:31 +0200

    delete console.log

--- server/controllers/UserCtrl.js
+++ server/controllers/UserCtrl.js
@@ -4,73 +4,70 @@ var UsersModel = require('./../models/users');
 const jwt = require('../modules/token');
 
 module.exports = {
-    login: function (user, res) {
-        console.log("1.\n", user)
+  login: function(user, res) {
+    UsersModel.userModel.findOne({ mail: user.mail, pwd: user.pwd }, function(err, result) {
+      if (err) {
+        res.status(404).send({ error: err });
+        return;
+      }
+      if (!result) {
+        res.status(404).send({ error: "Login et/ou mot de passe incorrect." })
 
-        UsersModel.userModel.findOne({mail: user.mail, pwd: user.pwd}, function (err, result) {
-            console.log("2.", err, "\n")
-            if (err) {
-                res.status(404).send({error: err});
-                return;
-            }
-            if (!result) {
-                res.status(404).send({error: "Login et/ou mot de passe incorrect."})
+      } else {
+        var token = jwt.createToken(result);
+        if (!token) {
+          res.status(200).send({ current: result.toDto(), token: token });
+        } else {
+          const err = "the server was unable to create a token.";
+          logger.error(err);
+          res.status(500).send(err);
+        }
+      }
+    }).catch(function(err) {
+      logger.error(err);
+      res.status(500).send(err);
+    })
+  },
 
-            } else {
-                var token = jwt.createToken(result);
-                if(!token){
-                    res.status(200).send({current: result.toDto(), token: token});
-                }else{
-                    const err = "the server was unable to create a token.";
-                    logger.error(err);
-                    res.status(500).send(err);
-                }
-            }
-        }).catch(function (err) {
-            logger.error(err);
-            res.status(500).send(err);
-        })
-    },
+  get: function(res) {
+    UsersModel.userModel.find({}).then(function(users) {
+      let tabU = [];
+      users.forEach(user => tabU.push(user.toDto()));
+      res.status(200).send({ message: tabU });
+    }).catch(function(err) {
+      logger.error(err);
+      res.status(500).send(err);
+    })
+  },
+  getById: function(id, res) {
+    //TODO connect to mongodb
+    res.status(200).send({ message: "Method to implements" });
+  },
+  getByEmail: function(email, res) {
+    //TODO connect to mongodb
+    res.status(200).send({ message: "Method to implements" });
 
-    get: function (res) {
-        UsersModel.userModel.find({}).then(function (users) {
-            let tabU = [];
-            users.forEach(user => tabU.push(user.toDto()));
-            res.status(200).send({message: tabU});
-        }).catch(function (err) {
-            logger.error(err);
-            res.status(500).send(err);
-        })
-    },
-    getById: function (id, res) {
-        //TODO connect to mongodb
-        res.status(200).send({message: "Method to implements"});
-    },
-    getByEmail: function (email, res) {
-        //TODO connect to mongodb
-        res.status(200).send({message: "Method to implements"});
+  },
+  create: function(user, res) {
+    let uTmp = new UsersModel.userModel();
+    uTmp.mail = user.mail;
+    uTmp.pwd = user.pwd;
+    uTmp.type = user.type;
+    uTmp.save().then(() => {
+      res.status(200).send({ message: uTmp.toDto() });
+    }).catch(function(err) {
+      logger.error(err);
+      res.status(500).send(err);
+    })
+  },
 
-    },
-    create: function (user, res) {
-        let uTmp = new UsersModel.userModel();
-        uTmp.mail = user.mail;
-        uTmp.pwd = user.pwd;
-        uTmp.type = user.type;
-        uTmp.save().then(() => {
-            res.status(200).send({message: uTmp.toDto()});
-        }).catch(function (err) {
-                logger.error(err);
-                res.status(500).send(err);
-            })
-    },
+  update: function(user, res) {
+    //TODO connect to mongodb
+    res.status(200).send({ message: "Method to implements" });
+  },
 
-    update: function (user, res) {
-        //TODO connect to mongodb
-        res.status(200).send({message: "Method to implements"});
-    },
-
-    delete: function (email, res) {
-        //TODO connect to mongodb
-        res.status(200).send({message: "Method to implements"});
-    }
+  delete: function(email, res) {
+    //TODO connect to mongodb
+    res.status(200).send({ message: "Method to implements" });
+  }
 };
