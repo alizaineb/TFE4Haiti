4e808beb3d14a6fe81107eceee5539f67654b5c6
Ricci Dorian <dorian.ricci@outlook.com>
2018-10-04 16:13:15 +0200

    added methode core (TODO change url + comportement)

--- client/src/app/_services/authentication.service.ts
+++ client/src/app/_services/authentication.service.ts
@@ -41,6 +41,16 @@ export class AuthenticationService {
   }
 
   resetPwd(pwd: String, confirmPwd: String) {
+    //TODO change url and param
+    return this.http.post<any>(`${environment.apiUrl}/users`, {mail: username, pwd: password})
+      .pipe(map(user => {
+        // login successful if there's a jwt token in the response
+        if (user && user.token) {
+          // store user details and jwt token in local storage to keep user logged in between page refreshes
+          localStorage.setItem('currentUser', JSON.stringify(user));
+        }
 
+        return user;
+      }));
   }
 }
