dcdf7ec24568c0597ef59d7cdf4a86c77eb6f896
Unknown <gaetan.ansotte@hotmail.com>
2018-10-14 01:39:09 +0200

    fix prev push

--- client/src/app/_models/note.ts
+++ client/src/app/_models/note.ts
@@ -1,10 +1,8 @@
-import { User } from '../_models';
-
 export class Note {
   public _id: string;
   public note: string;
   public user_id: string;
-  public user: User;
+  public user: any;
   public station_id: string;
   public updatedAt: Date;
   public createdAt: Date;
