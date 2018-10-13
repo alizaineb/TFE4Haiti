bd297bb2fac64b7485838f2293b901ce5d2696c5
Roland <roland.bailly@student.uclouvain.be>
2018-10-13 21:20:43 +0200

    fix connerie

--- client/src/app/_components/users/users.component.ts
+++ client/src/app/_components/users/users.component.ts
@@ -33,11 +33,6 @@ export class UsersComponent implements OnInit {
   private loadAllUsers() {
     let self = this;
     this.userService.getAll().pipe(first()).subscribe(result => {
-      for (let usr of result) {
-        usr.niceDateCreatedAt = self.toNiceDate(new Date(usr.created_at));
-        // @ts-ignore
-        usr.niceDateLastCo = self.toNiceDate(new Date(usr.last_seen));
-      }
       self.users = result;
     });
   }
