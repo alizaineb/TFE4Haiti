443b47a231d6819c06549e0728104022988ca284
Unknown <gaetan.ansotte@hotmail.com>
2018-10-25 12:48:18 +0200

    method user to push data

--- server/controllers/dataCtrl.js
+++ server/controllers/dataCtrl.js
@@ -9,6 +9,7 @@ const checkParam = require('./utils').checkParam;
  * @param {string} user L'id de l'utilisateur ayant inséré les données
  * ? Besoin d'un autre paramètre ?
  */
+
 exports.insertData = function(req, res) {
   // Vérifier que l'utilisateur peut insérer sur cette station
 
@@ -20,7 +21,6 @@ exports.insertData = function(req, res) {
   // ? si collision dans la date ?
 
   // Va falloir utiliser Promise.all(les promesses).then etc : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/all
-
   const datas = req.body;
   let tmp = [];
   for (let i = 0; i < datas.length; i++) {
@@ -35,24 +35,69 @@ exports.insertData = function(req, res) {
   dataModel.rainDataModel.insertMany(tmp, (err, docs) => {
     console.log(err);
     console.log(docs);
-    if(err){
+    if (err) {
       res.status(500).send('Les données n\'ont pas sur être insérer...');
-    }else{
+    } else {
       res.status(200).send();
     }
   })
-
-
 }
 
+//push();
+
+function push() {
+  const datas = [];
+  const id_user = "5bbdb325d7aec61a195afc96";
+  const id_station = "5bcf2b454d8f03350c5cf73e";
+  let ptr = 0;
+  for (let i = 0; i < 24; i++) {
+    for (let j = 0; j < 60; j++) {
+      let item = {};
+      item.id_station = id_station;
+      item.id_user = id_user;
+
+      let date2 = new Date('2018-10-12T' + padInt(i) + ':' + padInt(j) + ':00');
+      item.date = date2;
+      item.value = getRandomInt(80);
+      datas[ptr] = item;
+      ptr++
+    }
+  }
+  let tmp = [];
+  for (let i = 0; i < datas.length; i++) {
+    let d = datas[i];
+    let data = new dataModel.rainDataModel();
+    data.id_station = d.id_station;
+    data.id_user = d.id_user;
+    data.date = d.date;
+    data.value = d.value;
+    tmp.push(data);
+  }
+  dataModel.rainDataModel.insertMany(tmp, (err, docs) => {
+    console.log(err);
+    console.log(docs);
+    if (err) {
+      //res.status(500).send('Les données n\'ont pas sur être insérer...');
+    } else {
+      //res.status(200).send();
+    }
+  })
+}
+
+function padInt(val) {
+  if (val.toString().length <= 1)
+    return "0" + val.toString();
+  return val.toString();
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 /*
  * Méthode utilisée pour vérifier que la date passée corresponde à l'intervalle donnée.
  * @param {string} interval L'intervalle de la id_station
  * @param {Date} date La date d'entrée de la donnée
  */
-console.log("TEST : ");
-console.log(isCorrect("30mn", new Date('1995-12-17T03:30:00')));
-
 function isCorrect(interval, date) {
   // Intervalle existante : ['1min', '5min', '10min', '15min', '30min', '1h', '2h', '6h', '12h', '24h'];
   let val;
