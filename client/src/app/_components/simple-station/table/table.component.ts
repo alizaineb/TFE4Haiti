c26d9e017accc853f8d5d3634a15bc7bcde6869a
Unknown <gaetan.ansotte@hotmail.com>
2018-10-25 14:53:49 +0200

    Design detail

--- client/src/app/_components/simple-station/table/table.component.ts
+++ client/src/app/_components/simple-station/table/table.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Station } from '../../../_models/';
+import { AlertService, UserService } from '../../../_services/';
+import { StationsService } from '../../../_services/stations.service';
 
 @Component({
   selector: 'app-table',
@@ -10,7 +13,7 @@ export class TableComponent implements OnInit {
   @Input()
   private stationId: string;
 
-  constructor() { }
+  constructor(private stationService: StationsService, private alertService: AlertService) { }
 
   ngOnInit() {
   }
