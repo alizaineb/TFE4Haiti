5afd84b1f0a2715215a956e03d1cee1f427a0d4b
Unknown <gaetan.ansotte@hotmail.com>
2018-10-25 15:23:54 +0200

    design detail station

--- client/src/app/_components/simple-station/table/table.component.ts
+++ client/src/app/_components/simple-station/table/table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
 import { Station } from '../../../_models/';
 import { AlertService, UserService } from '../../../_services/';
 import { StationsService } from '../../../_services/stations.service';
@@ -13,9 +13,17 @@ export class TableComponent implements OnInit {
   @Input()
   private stationId: string;
 
+  private currentStation: Station;
+
   constructor(private stationService: StationsService, private alertService: AlertService) { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.stationService.getById(this.stationId).pipe().subscribe(station => {
+      this.currentStation = station
+    });
   }
 
 }
