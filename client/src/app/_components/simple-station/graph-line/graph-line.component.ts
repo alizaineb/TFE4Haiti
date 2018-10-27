9f37157b1abe8a3e41c16b614473a2a3cd1ec906
Unknown <gaetan.ansotte@hotmail.com>
2018-10-27 14:20:07 +0200

    comment crash line temporaire

--- client/src/app/_components/simple-station/graph-line/graph-line.component.ts
+++ client/src/app/_components/simple-station/graph-line/graph-line.component.ts
@@ -1,9 +1,9 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Chart} from 'chart.js';
-import {DataService} from '../../../_services/data.service';
-import {RainData} from '../../../_models/rainData';
+import { Component, Input, OnInit } from '@angular/core';
+//import {Chart} from 'chart.js';
+import { DataService } from '../../../_services/data.service';
+import { RainData } from '../../../_models/rainData';
 import * as Highcharts from 'highcharts/highstock';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-graph-line',
@@ -22,106 +22,106 @@ export class GraphLineComponent implements OnInit {
   listData: RainData[];
 
   constructor(private dataService: DataService,
-              private http: HttpClient) { }
+    private http: HttpClient) { }
 
   ngOnInit() {
-     this.dataService.getAllRainDataGraphLine(this.stationId).subscribe(data => {
-       console.log(data);
+    this.dataService.getAllRainDataGraphLine(this.stationId).subscribe(data => {
+      console.log(data);
 
-       Highcharts.setOptions({
-         lang: {
-           loading: 'Chargement...',
-           months: [
-             'Janvier', 'Février', 'Mars', 'Avril',
-             'Mai', 'Juin', 'Juillet', 'Août',
-             'Septembre', 'Octobre', 'Novembre', 'Décembre'
-           ],
-           weekdays: [
-             'Dimanche', 'Lundi', 'Mardi', 'Mercredi',
-             'Jeudi', 'Vendredi', 'Samedi'
-           ],
-           shortMonths: [
-             'Jan', 'Fev', 'Mar', 'Avr', 'Mai', 'Juin', 'Juil',
-             'Août', 'Sept', 'Oct', 'Nov', 'Dec'
-           ],
-         }
-       });
+      Highcharts.setOptions({
+        lang: {
+          loading: 'Chargement...',
+          months: [
+            'Janvier', 'Février', 'Mars', 'Avril',
+            'Mai', 'Juin', 'Juillet', 'Août',
+            'Septembre', 'Octobre', 'Novembre', 'Décembre'
+          ],
+          weekdays: [
+            'Dimanche', 'Lundi', 'Mardi', 'Mercredi',
+            'Jeudi', 'Vendredi', 'Samedi'
+          ],
+          shortMonths: [
+            'Jan', 'Fev', 'Mar', 'Avr', 'Mai', 'Juin', 'Juil',
+            'Août', 'Sept', 'Oct', 'Nov', 'Dec'
+          ],
+        }
+      });
 
-       // Create the chart
-       this.highChart = Highcharts.stockChart('container', {
+      // Create the chart
+      this.highChart = Highcharts.stockChart('container', {
 
-         rangeSelector: {
-           inputEnabled: false,
-           selected: 1,
-           buttons: [
-             {
-               type: 'minute',
-               count: 5,
-               text: '5min'
-             },
-             {
-             type: 'minute',
-             count: 60,
-             text: '1h'
-           }, {
-             type: 'day',
-             count: 1,
-             text: '1d'
-           }, {
-             type: 'week',
-             count: 1,
-             text: '1w'
-           }, {
-             type: 'month',
-             count: 1,
-             text: '1m'
-           }, {
-             type: 'year',
-             count: 1,
-             text: '1y'
-           }, {
-             type: 'all',
-             text: 'All'
-           }]
-         },
+        rangeSelector: {
+          inputEnabled: false,
+          selected: 1,
+          buttons: [
+            {
+              type: 'minute',
+              count: 5,
+              text: '5min'
+            },
+            {
+              type: 'minute',
+              count: 60,
+              text: '1h'
+            }, {
+              type: 'day',
+              count: 1,
+              text: '1d'
+            }, {
+              type: 'week',
+              count: 1,
+              text: '1w'
+            }, {
+              type: 'month',
+              count: 1,
+              text: '1m'
+            }, {
+              type: 'year',
+              count: 1,
+              text: '1y'
+            }, {
+              type: 'all',
+              text: 'All'
+            }]
+        },
 
-         title: {
-           text: 'AAPL Stock Price'
-         },
-         xAxis: {
-           type: 'datetime'
-         },
-         series: [{
-           name: 'AAPL',
-           data: data,
-           tooltip: {
-             valueDecimals: 2
-           }
-         }]
-       });
-     });
+        title: {
+          text: 'AAPL Stock Price'
+        },
+        xAxis: {
+          type: 'datetime'
+        },
+        series: [{
+          name: 'AAPL',
+          data: data,
+          tooltip: {
+            valueDecimals: 2
+          }
+        }]
+      });
+    });
 
-/*
-    this.data = [
-      [
-        1540552440000,
-        0.2
-      ],
-      [
-        1540552500000,
-        0
-      ],
-      [
-        1540552560000,
-        0.1
-      ],
-      [
-        1540552620000,
-        0.2
-      ],
-    ];
+    /*
+        this.data = [
+          [
+            1540552440000,
+            0.2
+          ],
+          [
+            1540552500000,
+            0
+          ],
+          [
+            1540552560000,
+            0.1
+          ],
+          [
+            1540552620000,
+            0.2
+          ],
+        ];
 
-    */
+        */
   }
 
 }
