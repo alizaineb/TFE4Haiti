3e9aee09a600b0e0f69f0199aed3ae84dc3a7f4f
Unknown <gaetan.ansotte@hotmail.com>
2018-11-01 13:43:14 +0100

    fix old_Data undef

--- server/controllers/dataCtrl.js
+++ server/controllers/dataCtrl.js
@@ -175,13 +175,13 @@ exports.refuseAwaiting = function(req, res) {
     return res.status(400).send("Information manquante(s)");
   }
   dataModel.RainDataAwaitingModel.findById(id, (err, rainDataAwaiting) => {
-    logger.info("[DATACTRL] refuseAwaiting.findbyid : ", id," - ",  rainDataAwaiting);
+    logger.info("[DATACTRL] refuseAwaiting.findbyid : ", id, " - ", rainDataAwaiting);
     if (err) {
       logger.error("[DATACTRL] refuseAwaiting : ", err)
       return res.status(500).send("Erreur lors de la recupération de la donnée.")
     } else {
       dataModel.RainDataAwaitingModel.deleteOne({ _id: id }).then(() => {
-        if(rainDataAwaiting.type == "file"){
+        if (rainDataAwaiting.type == "file") {
           const filePath = path.join(nconf.get('uploadFolder'), rainDataAwaiting.value);
           fs.unlink(filePath, (err) => {
             logger.error('[IMPORTFILE] remove : ', err);
@@ -242,10 +242,10 @@ exports.getRainDataGraphLineOneMonth = function(req, res) {
 
     month = parseInt(month, 10);
     //Date month begin to 0 !
-    month-=1;
+    month -= 1;
 
     let dateMin = new Date(Date.UTC(year, month, 1, 0, 0, 0, 0));
-    let dateMax = new Date(Date.UTC(year, dateMin.getMonth()+1, 0, 23, 23, 59, 0));
+    let dateMax = new Date(Date.UTC(year, dateMin.getMonth() + 1, 0, 23, 23, 59, 0));
 
 
     dataModel.rainDataModel.find({
@@ -552,7 +552,7 @@ exports.updateData = function(req, res) {
           let dataToSend = new dataModel.RainDataAwaitingModel();
           dataToSend.id_station = req.params.id_station;
           dataToSend.id_user = req.token_decoded.id;
-          dataToSend.id_old_data = req.id_data;
+          dataToSend.id_old_data = id_data;
           dataToSend.date = rainData.date;
           dataToSend.type = state.UPDATE;
           dataToSend.value = data;
@@ -588,7 +588,7 @@ exports.updateData = function(req, res) {
       });
     }
   }
-  // Si data on désire passer la donnée à vide
+  // Si data ==> vide
   else if (req.body.data === undefined || req.body.data === '') {
     // Si il manque la data à laquelle if faut update
     dataModel.rainDataModel.findById(id_data, (err, rainData) => {
@@ -603,8 +603,9 @@ exports.updateData = function(req, res) {
         dataToSend.id_station = req.params.id_station;
         dataToSend.id_user = req.token_decoded.id;
         dataToSend.date = rainData.date;
-        dataToSend.id_old_data = req.id_data;
+        dataToSend.id_old_data = id_data;
         dataToSend.type = state.UPDATE;
+        console.log(dataToSend);
         dataToSend.save().then(() => {
           return res.status(201).send();
         }).catch(function(err) {
