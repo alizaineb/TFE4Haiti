c1edc4eedd7d008029532bb66424ea5c7ff6bd12
gaet <gaetan.ansotte@hotmail.com>
2018-11-14 15:42:55 +0100

    date fix

--- client/src/app/_components/stations/import-data/station-import-data.component.ts
+++ client/src/app/_components/stations/import-data/station-import-data.component.ts
@@ -104,7 +104,9 @@ export class StationImportDataComponent implements OnInit {
         tmp.value = this.data[i].value;
         //console.log(this.data[i].date);
         //console.log(`${this.data[i].date}T${this.data[i].time.hour}:${this.data[i].time.time | this.data[i].time['minute']}:00`);
-        tmp.date = new Date(`${this.data[i].date}T${this.minTwoDigits(this.data[i].time.hour)}:${this.minTwoDigits(this.data[i].time.time) | this.minTwoDigits(this.data[i].time['minute'])}:00Z`);
+        console.log(this.data[i].time);
+        tmp.date = new Date(`${this.data[i].date}T${this.minTwoDigits(this.data[i].time.hour)}:${this.minTwoDigits(this.data[i].time['minute'])}:00Z`);
+        console.log(tmp.date);
         // tmp.date = new Date(Date.UTC(tmp.date.getFullYear(), tmp.date.getMonth(), tmp.date.getDate(), tmp.date.getHours(), tmp.date.getMinutes(), tmp.date.getSeconds()));
         dataToSend.push(tmp);
       }
