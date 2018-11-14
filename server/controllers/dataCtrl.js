021e84463e1e7083d09486f67bea4b942f701002
Ricci Dorian <dorian.ricci@outlook.com>
2018-11-14 15:14:02 +0100

    test date

--- server/controllers/dataCtrl.js
+++ server/controllers/dataCtrl.js
@@ -781,7 +781,8 @@ exports.importManualData = function(req, res) {
             let data = new dataModel.RainDataAwaitingModel();
             data.id_station = station._id;
             data.id_user = user._id;
-            data.date = new Date(Date.UTC(d.date.getFullYear(), d.date.getMonth(), d.date.getDate(), d.date.getHours(), d.date.getMinutes(), d.date.getSeconds()));
+            data.date = new Date(d.date);
+            data.date = new Date(Date.UTC(data.date.getFullYear(), data.date.getMonth(), data.date.getDate(), data.date.getHours(), data.date.getMinutes(), data.date.getSeconds()));
             data.value = d.value;
             data.type = state.INDIVIDUAL;
             tmp.push(data);
