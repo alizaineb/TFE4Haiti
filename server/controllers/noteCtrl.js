06bd70f80d18afc331167fb03d25dc147d207f33
gaet <gaetan.ansotte@hotmail.com>
2018-11-16 13:27:25 +0100

    enhance comms

--- server/controllers/noteCtrl.js
+++ server/controllers/noteCtrl.js
@@ -14,20 +14,24 @@ const errors = require('./utils').errors;
 
 /**
  * create - Permet de créer une note pour une station
- * PRE : La station a été vérifiée par le middleware hasAccesToStation, et nous sommes donc sûrs qu'elle existe
+ * PRE : La station a été vérifiée par le middleware hasAccesToStation
+ *       nous sommes donc sûrs qu'elle existe 
+ *
  * @param {request} req Requête du client
+ * @param {string} req.body.station_id l'id de la station pour laquelle il faut ajouter une note
+ * @param {string} req.body.note La note a ajouter
+ * @param {string} req.token_decoded.id L'id de l'utilisateur qui crée la note
  * @param {response} res Réponse renvoyée au client
  *                       400 : Paramètre manquant
  *                       500 : erreur serveur
  * @return     201 : la note ajoutée en base de données
  */
 exports.create = function(req, res) {
-  let note = req.body;
   let nTmp = new NoteModel.noteModel();
 
-  nTmp.station_id = note.station_id;
+  nTmp.station_id = req.body.station_id;
   nTmp.user_id = req.token_decoded.id;
-  nTmp.note = note.note;
+  nTmp.note = req.body.note;
   if (note.note.trim().length === 0) {
     return res.status(400).send("La note ne peut pas être vide");
   }
@@ -46,9 +50,10 @@ exports.create = function(req, res) {
  * get - Récupère toutes les notes liées à une station
  *
  * @param {request} req Requête du client
+ * @param {strçing} req.params.station_id L'id de la station pour laquelle il faut récupérer ses notes
  * @param {response} res Réponse renvoyée au client
  *                       500 : Erreur serveur
- * @return {type}     200 :  les notes de la station
+ * @return {type}    200 :  les notes de la station
  */
 exports.get = function(req, res) {
   NoteModel.noteModel.find({ station_id: req.params.station_id },
