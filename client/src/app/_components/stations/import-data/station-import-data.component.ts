b96d80b3c969f66822a56609f2a8980ae1e6010a
gaet <gaetan.ansotte@hotmail.com>
2018-11-26 13:43:44 +0100

    fix bug windows

--- client/src/app/_components/stations/import-data/station-import-data.component.ts
+++ client/src/app/_components/stations/import-data/station-import-data.component.ts
@@ -1,12 +1,12 @@
-import {Component, OnInit} from '@angular/core';
-import {Station} from '../../../_models';
-import {StationsService} from '../../../_services/stations.service';
-import {ActivatedRoute, Router} from '@angular/router';
-import {RainData} from '../../../_models/rainData';
-import {AlertService} from '../../../_services';
-import {NgbTimepickerConfig} from '@ng-bootstrap/ng-bootstrap';
-import {LocalstorageService} from '../../../_services/localstorage.service';
-import {Location} from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Station } from '../../../_models';
+import { StationsService } from '../../../_services/stations.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RainData } from '../../../_models/rainData';
+import { AlertService } from '../../../_services';
+import { NgbTimepickerConfig } from '@ng-bootstrap/ng-bootstrap';
+import { LocalstorageService } from '../../../_services/localstorage.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-import-data',
@@ -28,11 +28,11 @@ export class StationImportDataComponent implements OnInit {
 
 
   constructor(private stationService: StationsService,
-              private localStorageService: LocalstorageService,
-              private alertService: AlertService,
-              private route: ActivatedRoute,
-              private configTimePicker: NgbTimepickerConfig,
-              private _location: Location
+    private localStorageService: LocalstorageService,
+    private alertService: AlertService,
+    private route: ActivatedRoute,
+    private configTimePicker: NgbTimepickerConfig,
+    private _location: Location
   ) {
     this.configTimePicker.spinners = false;
   }
@@ -67,14 +67,14 @@ export class StationImportDataComponent implements OnInit {
     this._location.back();
   }
 
-  private addData(date = '', time = {hour: 0, minute: 0}, value = 0) {
-    this.data.push({date: date, time: time, value: value});
+  private addData(date = '', time = { hour: 0, minute: 0 }, value = 0) {
+    this.data.push({ date: date, time: time, value: value });
   }
 
 
   isSelectetd(item: string) {
     const res = this.selectedZone === item;
-    return {'col-md-10': res, 'col-md-2': !res, 'notselected': !res};
+    return { 'col-md-10': res, 'col-md-2': !res, 'notselected': !res };
   }
 
   changeZone(item: string) {
@@ -152,7 +152,7 @@ export class StationImportDataComponent implements OnInit {
   onFileSelected($event) {
     const self = this;
     const f = <File>$event.target.files[0];
-    if (f.type.toLowerCase() === 'text/csv') {
+    if (f.type.toLowerCase() === 'text/csv' || f.type.toLowerCase() === 'application/vnd.ms-excel') {
       if ((f.size / 1024) > 2048) { // passer la size en ko et la comparer a 2mo en ko
         self.alertService.error('La taille max est de 2Mo');
       } else {
