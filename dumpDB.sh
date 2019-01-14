8c1ca5a4f24493d464fdf96986879872712d2376
Ricci Dorian <dorian.ricci@outlook.com>
2019-01-14 18:50:51 +0100

    commit Selenium tests

--- dumpDB.sh
+++
@@ -1,3 +0,0 @@
-mongodump -h ds117164.mlab.com:17164 -d tfe4haitiprod -u tfe4haiti -p IL0veDodo -o /home/dorian/backupDB
-mongorestore --drop -h ds117164.mlab.com:17164 -d tfe4hai-htiprod -u tfe4haiti -p IL0veDodo /home/dorian/backupDB
-
